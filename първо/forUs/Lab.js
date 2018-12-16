395fd62798d800be566a92f09789cdbe7a4dd442
Asen Nikolaev <asen.nikolaev@nevexis.com>
2018-12-16 18:08:20 +0200

    current status

---
+++ първо/forUs/Lab.js
@@ -0,0 +1,34 @@
+var url;
+debugger;
+if (document.title == "Грижа за тяло")
+    url = "body.xml";
+else if (document.title == "Грижа за коса")
+    url = "hair.xml";
+else if (document.title == "Грижа за лице")
+    url = "skin.xml";
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200)
+        displayXML(this);
+};
+xhttp.open("GET", url, true);
+xhttp.send();
+function displayXML(xml) {
+    alert("sd");
+    var xmlDoc = xml.responseXML;
+    var src = xmlDoc.getElementsByTagName("src");
+    var onclick = xmlDoc.getElementsByTagName("onclick");
+    var alt = xmlDoc.getElementsByTagName("alt");
+    var item, srcValue, onclickValue, altValue;
+    var articles = document.querySelector("article");
+    for (var i = 0; i < src.length; i++) {
+        srcValue = src[i].childNodes[0].nodeValue;
+        onclickValue = onclick[i].childNodes[0].nodeValue;
+        altValue = alt[i].childNodes[0].nodeValue;
+        item = document.createElement("img");
+        item.setAttribute("src", srcValue);
+        item.setAttribute("onclick", onclickValue);
+        item.setAttribute("alt", altValue);
+        articles.appendChild(item);
+    }
+}
